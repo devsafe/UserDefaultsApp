ba81d2830ebc776e8e09e567507f0d521ccb1029
Rafinad <rafinad@outlook.com>
2021-05-07 20:20:24 +0300

    Initial Commit

---
+++ UserDefaultsApp/UserDefaultsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsAppApp.swift
+//  UserDefaultsApp
+//
+//  Created by Сахар Кубический on 07.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
